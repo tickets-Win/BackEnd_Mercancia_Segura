452d6eabf8472aed14d427ab006674aa99af9afd
Enrique <e.escandon@winsef.mx>
2026-02-03 13:12:13 -0600

    Modelo RestApi TipoCorreo

---
+++ TemplateNetCore-main/MercanciaSegura.RestAPI/Controllers.Implementation/Catalogos/TipoCorreoApi.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MercanciaSegura.DOM.ApplicationDbContext;
+using MercanciaSegura.DOM.Modelos.Cliente;
+using MercanciaSegura.RestAPI.Models.Catalogos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercanciaSegura.RestAPI.Controllers.Implementation.Catalogos
+{
+    public class TipoCorreoApiController : Controllers.Catalogos.TipoCorreoApiControllerBase
+    {
+        private readonly ServiceDbContext _context;
+
+        public TipoCorreoApiController(ServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        private TipoCorreoResponse MapToResponse(TipoCorreo t)
+        {
+            return new TipoCorreoResponse
+            {
+                TipoCorreoId = t.TipoCorreoId,
+                Tipo = t.Tipo
+            };
+        }
+
+        public override async Task<IActionResult> GetTipoCorreoApiAsync(string version)
+        {
+            var tiposCorreo = await _context.TipoCorreo.ToListAsync();
+            return Ok(tiposCorreo.Select(MapToResponse));
+        }
+    }
+}
---
+++ TemplateNetCore-main/MercanciaSegura.RestAPI/Controllers/Catalogos/TipoCorreoApi.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using MercanciaSegura.RestAPI.Attributes;
+using MercanciaSegura.RestAPI.Controllers.Base;
+using MercanciaSegura.RestAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace MercanciaSegura.RestAPI.Controllers.Catalogos
+{
+    [ApiController]
+    public abstract class TipoCorreoApiControllerBase : ServiceBaseController
+    {
+        [HttpGet]
+        [Route("/{version:apiVersion}/tipoCorreo")]
+        [ValidateModelState]
+        [SwaggerOperation("GetTipoCorreo")]
+        [SwaggerResponse(statusCode: 200, description: "OK")]
+        [SwaggerResponse(statusCode: 400, type: typeof(InlineResponse400),
+            description: "Response to client error status code")]
+        [SwaggerResponse(statusCode: 401, type: typeof(InlineResponse400),
+            description: "Response to client error status code")]
+        [SwaggerResponse(statusCode: 404, type: typeof(InlineResponse400),
+            description: "Response to client error status code")]
+        public abstract Task<IActionResult> GetTipoCorreoApiAsync(
+            [FromRoute][Required] string version);
+    }
+}
