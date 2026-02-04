26808add00cbc0b7e44466bfef5db1019b0e6e04
Enrique <e.escandon@winsef.mx>
2026-02-04 11:12:02 -0600

    mostrar tipo y no id de correos y clientevendedor

---
+++ TemplateNetCore-main/MercanciaSegura.RestAPI/Controllers.Implementation/CorreosApi.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MercanciaSegura.DOM.ApplicationDbContext;
+using MercanciaSegura.DOM.Modelos.Cliente;
+using MercanciaSegura.RestAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercanciaSegura.RestAPI.Controllers.Implementation
+{
+    public class CorreosApiController : Controllers.CorreosApiControllerBase
+    {
+        private readonly ServiceDbContext _context;
+
+        public CorreosApiController(ServiceDbContext context)
+        {
+            _context = context;
+        }
+        public override async Task<IActionResult> GetCorreosByClienteAsync(
+            string version,
+            int clienteId)
+        {
+            var correos = await _context.Correos
+                .Where(c => c.ClienteId == clienteId)
+                .Include(c => c.TipoCorreo)
+                .Select(c => new CorreosResponse
+                {
+                    CorreoId = c.CorreoId,
+                    Correo = c.Correo,
+                    TipoCorreo = c.TipoCorreo != null
+                        ? c.TipoCorreo.Tipo
+                        : null
+                })
+                .ToListAsync();
+
+            return Ok(correos);
+        }
+    }
+}
